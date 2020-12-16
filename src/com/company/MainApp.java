ec4204d27bc637b9418a1130c07600b76e0aecc6
Karol98 <44203620+Karol98@users.noreply.github.com>
2020-12-16 18:11:01 +0100

    Update MainApp.java

--- src/com/company/MainApp.java
+++ src/com/company/MainApp.java
@@ -5,6 +5,26 @@ import com.company.Algorithms.DES;
 import com.company.Menu.Keyin;
 import com.company.Menu.TextColor;
 
+/**
+ * Autors
+ * Mario Gliwa s18866
+ * Karol Skwierawski s18497
+ *
+ *
+ *
+ * DES - is a block cipher algorithm that takes plain text in blocks of 64 bits and converts them to ciphertext using
+ * keys of 48 bits. It is a symmetric key algorithm, which means that the same key is used for encrypting and decrypting data
+ *
+ * 3DES - Triple DES is a encryption technique which uses three instance of DES on same plain text. It uses there different
+ * types of key choosing technique in first all used keys are different and in second two keys are same and one ise different and
+ * in third all keys are same.
+ *
+ * AES - AES is block cipher capable of handling 128 bit blocks, using keys sized at 128, 192, and 256 bits.
+ * Each cipher encrypts and decrypts data in blocks of 128 bits using cryptographic keys of 128-, 192- and 256-bits, respectively.
+ * It uses the same key for encrypting and decrypting, so the sender and the receiver must both know — and use — the same secret key.
+ *
+ */
+
 public class MainApp {
 
     public static void main(String[] args) {
@@ -17,9 +37,9 @@ public class MainApp {
         System.out.println("|         BSI-Project-Calculator                    |");
         System.out.println("=====================================================");
         System.out.println("| Options:                                          |");
-        System.out.println("|        1. AES encryption/decryption               |");
-        System.out.println("|        2. DES encryption/decryption               |");
-        System.out.println("|        3. 3DES encryption/decryption              |");
+        System.out.println("|        1. 3DES encryption/decryption              |");
+        System.out.println("|        2. AES encryption/decryption               |");
+        System.out.println("|        3. DES encryption/decryption               |");
         System.out.println("|        4. EXIT                                    |");
         System.out.println("=====================================================");
         swValue = Keyin.inInt(TextColor.GREEN + " Select option: ");
@@ -32,7 +52,7 @@ public class MainApp {
             case 2:
                 final String secretKey = "ssshhhhhhhhhhh!!!!";
 
-                String originalString = "howtodoinjava.com";
+                String originalString = "BSI-Cryptography";
                 String encryptedString = AES.encrypt(originalString, secretKey) ;
                 String decryptedString = AES.decrypt(encryptedString, secretKey) ;
 
